cecf3a216da1d6519d39ecb8cd27204af7fc90c3
firtname lastname <pantota@hotmail.com>
2011-10-18 15:38:34 +0100

    made some changes in Product class

--- lib/product.rb
+++ lib/product.rb
@@ -2,7 +2,7 @@ class Product
 
   #id, barcode, name, brand, description, category, price, picture, location
 
-  def initialize(name, description, location, id, barcode)
+  def initialize(id, barcode, name, brand, description, category, price, picture, location)
     if(id == nil)
           raise ArgumentError , "id parameter is nil"
      end
@@ -18,15 +18,33 @@ class Product
     if (barcode == nil)
        raise ArgumentError , "barcode parameter is nil"
     end
+    if(brand == nil)
+      raise ArgumentError , "brand parameter is nil"
+    end
+    if(category == nil)
+      raise ArgumentError , "category parameter is nil"
+    end
+
+    if(price == nil)
+      raise ArgumentError , "price parameter is nil"
+    end
+    if(picture == nil)
+      raise ArgumentError , "picture parameter is nil"
+    end
+
 
     @name = name
     @description = description
     @location  = location
     @id = id
     @barcode = barcode
+    @brand = brand
+    @category = category
+    @picture = picture
+    @price = price
   end
 
- attr_accessor :name, :description, :location, :id, :barcode
+ attr_accessor :id, :barcode, :name, :brand, :description, :category, :price, :picture, :location
 
 
 
