837726f5a3e0bf1dbd749d3e331397cf8c8bac27
Tigers <renzo.bertuzzi@postgrad.manchester.ac.uk>
2011-10-18 12:11:24 +0100

    passes test: file exists and not empty

--- lib/storage.rb
+++ lib/storage.rb
@@ -1,8 +1,19 @@
 class Storage
   attr_reader :products, :quantity
 
+  PRODUCTS_FILE = "../products.csv"
+
+  #products file: id, barcode, name, brand, description, category, price, picture, location
+  #quiantity file: id, quantity
+
   def initialize
     @products = []
     @quantity = {}
+  end
+
+  def load_products_file filename=PRODUCTS_FILE
+    File.open(filename, 'r').each do |line|
+      puts line #.split(",")
+    end
   end
 end
--- spec/storage_spec.rb
+++ spec/storage_spec.rb
@@ -2,6 +2,19 @@ require 'rspec'
 require '../lib/storage.rb'
 
 describe Storage do
+  let(:filename) {"../products_test.csv"}
+
+  before :all do
+    create_dummy_data
+  end
+
+
+  def create_dummy_data
+     File.open(filename, 'w') do |f|
+       f.puts("1,12345678,lcd tv,sony,lcd tv,TV,1000,1.jpg,level2")
+       f.puts("2,55555555,bike XYZ,oxford,super bike,SPORTS,500,2.jpg,level1")
+     end
+  end
 
   it "products should be empty when the object is created the first time" do
     subject.products.should be_empty
@@ -10,5 +23,30 @@ describe Storage do
   it "quantity be empty when the object is created the first time" do
     subject.quantity.should be_empty
   end
+
+  it "should raise and exception if the file products.csv doesnt exist" do
+    lambda {
+      subject.load_products_file 'anyfile.csv'
+    }.should raise_exception()
+  end
+
+  it "should not raise and exception if the file products.csv exists" do
+    lambda {
+      subject.load_products_file filename
+    }.should_not raise_exception()
+  end
+
+  #it "should check the file products.csv exists and is not empty" do
+  #  lambda {
+  #    subject.load_products_file()
+  #  }.should_not raise_exception()
+
+  #  subject.should have_at_least(1).products
+  #end
+
+ # it "should load the contents of the file into array products" do
+ #   subject.load_products_file()
+ #   subject.should have(2).products
+ # end
 
 end
