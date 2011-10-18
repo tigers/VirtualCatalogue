b93d8e32db24dc50cd869979fdfde4c05eed923a
Tigers <renzo.bertuzzi@postgrad.manchester.ac.uk>
2011-10-18 14:09:10 +0100

    quantity is empty when the object is created

--- spec/storage_spec.rb
+++ spec/storage_spec.rb
@@ -3,43 +3,65 @@ require '../lib/storage.rb'
 
 describe Storage do
   let(:filename) {"../products_test.csv"}
+  let(:filename_quantity) {"../quantity_test.csv"}
 
   before :all do
-    create_dummy_data
+    create_dummy_products
+    create_dummy_quantity
   end
 
 
-  def create_dummy_data
+  def create_dummy_products
      File.open(filename, 'w') do |f|
        f.puts("1,12345678,lcd tv,sony,lcd tv,TV,1000,1.jpg,level2")
        f.puts("2,55555555,bike XYZ,oxford,super bike,SPORTS,500,2.jpg,level1")
      end
   end
 
-  it "products should be empty when the object is created the first time" do
-    subject.products.should be_empty
+  def create_dummy_quantity
+    File.open(filename_quantity,'w') do |fq|
+      fq.puts("1,5")
+      fq.puts("2,15")
+    end
   end
 
-  it "quantity be empty when the object is created the first time" do
-    subject.quantity.should be_empty
+  context "About products" do
+    it "products should be empty when the object is created the first time" do
+      subject.products.should be_empty
+    end
+
+    it "quantity be empty when the object is created the first time" do
+      subject.quantity.should be_empty
+    end
+
+    it "should raise and exception if the file products.csv doesn't exist" do
+      lambda {
+        subject.load_products_file 'anyfile.csv'
+      }.should raise_exception()
+    end
+
+    it "should not raise and exception if the file products.csv exists" do
+      lambda {
+        subject.load_products_file filename
+      }.should_not raise_exception()
+    end
+
+
+    it "should load the contents of the file into array products" do
+      subject.load_products_file()
+      subject.should have(2).products
+    end
   end
 
-  it "should raise and exception if the file products.csv doesnt exist" do
-    lambda {
-      subject.load_products_file 'anyfile.csv'
-    }.should raise_exception()
-  end
 
-  it "should not raise and exception if the file products.csv exists" do
-    lambda {
-      subject.load_products_file filename
-    }.should_not raise_exception()
-  end
+  context "About quantity" do
+
+    it "quantity should be empty when the object is created" do
+      subject.quantity.should be_empty
+    end
 
 
-  it "should load the contents of the file into array products" do
-    subject.load_products_file()
-    subject.should have(2).products
   end
+
 
 end
