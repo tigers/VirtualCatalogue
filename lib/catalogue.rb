66b20aa4f72f974992cb53b1f756631039ee3103
George William Howarth <howarthg@uf061.cs.man.ac.uk>
2011-10-18 18:26:27 +0100

    completed first full implementation of Catalogue class, tested new implementation

--- lib/catalogue.rb
+++ lib/catalogue.rb
@@ -12,16 +12,60 @@ class Catalogue
       raise ArgumentError, "'product' cannot be nil"
     end
 
-    # Check for existing product
-    if @products.index{|p| p.id==product.id} != nil
-      raise ArgumentError, "'product' already exist"
+    if @products.index{|p| p.id == product.id} != nil
+      raise ArgumentError, "'product' already exists"
     end
 
     @products.push(product)
   end
 
   def search(search_term)
+    if @products.count == 0
+      return []
+    end
 
+    search_result = []
+    search_term_str = search_term.to_s
+
+    @products.each do
+      |p|
+      if p.id.to_s.include? search_term_str
+        search_result.push(p)
+        next
+      end
+
+      if p.barcode.to_s.include? search_term_str
+        search_result.push(p)
+        next
+      end
+
+      if p.name.include? search_term_str
+        search_result.push(p)
+        next
+      end
+
+      if p.brand.include? search_term_str
+        search_result.push(p)
+        next
+      end
+
+      if p.description.include? search_term_str
+        search_result.push(p)
+        next
+      end
+
+      if p.category.include? search_term_str
+        search_result.push(p)
+        next
+      end
+
+      if p.location.include? search_term_str
+        search_result.push(p)
+        next
+      end
+    end
+
+    return search_result
   end
 
   def remove_product(product_id)
@@ -29,12 +73,10 @@ class Catalogue
       raise ArgumentError, "'product_id' cannot be nil"
     end
 
-    # Check for existing product
-    if @products.index{|p| p.id==product_id} == nil
+    if @products.index{|p| p.id == product_id} == nil
       raise ArgumentError, "'product_id' does not exist"
     end
 
-    # Remove appropriate product
-    @products.delete_if{|p| p.id==product_id}
+    @products.delete_if{|p| p.id == product_id}
   end
 end
--- spec/catalogue_spec.rb
+++ spec/catalogue_spec.rb
@@ -16,30 +16,28 @@ describe "Catalogue" do
     }.should raise_exception()
   end
 
-  # need to implement in Catalogue class
   it "should not add a product if the id of the given product already exists" do
     # Pre-condition
     catalogue = Catalogue.new
-    existing_product = Product.new("", "", "", 0, 01234)
+    existing_product = Product.new(0, "", "", "", "", "", "", "", "")
     catalogue.products.push(existing_product)
 
     # do test
-    new_product = Product.new("", "", "", 0, 01234)
+    new_product = Product.new(0, "", "", "", "", "", "", "", "")
     lambda{
 
       catalogue.add_product(new_product)
     }.should raise_exception()
-
   end
 
   it "should add a product if the id of the given product does not exist" do
     # Pre-condition
     catalogue = Catalogue.new
-    given_product = Product.new("", "", "", 0, 01234)
+    given_product = Product.new(0, "", "", "", "", "", "", "", "")
     catalogue.products.push(given_product)
 
     # do test
-    new_product = Product.new("", "", "", 1, 01234)
+    new_product = Product.new(1, "", "", "", "", "", "", "", "")
     catalogue.add_product(new_product)
     catalogue.products.index{|p| p.id == 1}.should_not == nil
   end
@@ -51,18 +49,113 @@ describe "Catalogue" do
     }.should raise_exception()
   end
 
-  # need to implement in Catalogue class
   it "should not attempt to remove a product if the id of the given product does not exist" do
     # Pre-condition
     catalogue = Catalogue.new
-    existing_product = Product.new("", "", "", 0, 01234)
+    existing_product = Product.new(0, "", "", "", "", "", "", "", "")
     catalogue.products.push(existing_product)
 
-    product_to_be_deleted = Product.new("","","", 3, 3334)
-     lambda {
+    product_to_be_deleted = Product.new(3, "", "", "", "", "", "", "", "")
+    lambda {
       catalogue.remove_product(product_to_be_deleted )
     }.should raise_exception()
-
   end
 
+  it "should return an array of products of which each product's id contains the search term" do
+    catalogue = Catalogue.new
+    existing_product1 = Product.new(0, "", "", "", "", "", "", "", "")
+    existing_product2 = Product.new(1, "", "", "", "", "", "", "", "")
+    existing_product3 = Product.new(2, "", "", "", "", "", "", "", "")
+    existing_product4 = Product.new(3, "", "", "", "", "", "", "", "")
+    catalogue.products.push(existing_product1)
+    catalogue.products.push(existing_product2)
+    catalogue.products.push(existing_product3)
+    catalogue.products.push(existing_product4)
+
+    catalogue.search(3).count.should == 1
+  end
+
+  it "should return an array of products of which each product's barcode contains the search term" do
+    catalogue = Catalogue.new
+    existing_product1 = Product.new(0, 12345, "", "", "", "", "", "", "")
+    existing_product2 = Product.new(1, 54321, "", "", "", "", "", "", "")
+    existing_product3 = Product.new(2, 12867, "", "", "", "", "", "", "")
+    existing_product4 = Product.new(3, 12555, "", "", "", "", "", "", "")
+    catalogue.products.push(existing_product1)
+    catalogue.products.push(existing_product2)
+    catalogue.products.push(existing_product3)
+    catalogue.products.push(existing_product4)
+
+    catalogue.search(12).count.should == 3
+  end
+
+  it "should return an array of products of which each product's name contains the search term" do
+    catalogue = Catalogue.new
+    existing_product1 = Product.new(0, "", "T850", "", "", "", "", "", "")
+    existing_product2 = Product.new(1, "", "X434", "", "", "", "", "", "")
+    existing_product3 = Product.new(2, "", "T8000", "", "", "", "", "", "")
+    existing_product4 = Product.new(3, "", "D310", "", "", "", "", "", "")
+    catalogue.products.push(existing_product1)
+    catalogue.products.push(existing_product2)
+    catalogue.products.push(existing_product3)
+    catalogue.products.push(existing_product4)
+
+    catalogue.search("T").count.should == 2
+  end
+
+  it "should return an array of products of which each product's brand contains the search term" do
+    catalogue = Catalogue.new
+    existing_product1 = Product.new(0, "", "T850", "Sony", "", "", "", "", "")
+    existing_product2 = Product.new(1, "", "X434", "LG", "", "", "", "", "")
+    existing_product3 = Product.new(2, "", "T8000", "Sony", "", "", "", "", "")
+    existing_product4 = Product.new(3, "", "D310", "Hitachi", "", "", "", "", "")
+    catalogue.products.push(existing_product1)
+    catalogue.products.push(existing_product2)
+    catalogue.products.push(existing_product3)
+    catalogue.products.push(existing_product4)
+
+    catalogue.search("Sony").count.should == 2
+  end
+
+  it "should return an array of products of which each product's description contains the search term" do
+    catalogue = Catalogue.new
+    existing_product1 = Product.new(0, "", "T850", "Sony", "LCD TV", "", "", "", "")
+    existing_product2 = Product.new(1, "", "X434", "LG", "Plasma TV", "", "", "", "")
+    existing_product3 = Product.new(2, "", "T8000", "Sony", "Plasma TV", "", "", "", "")
+    existing_product4 = Product.new(3, "", "D310", "Hitachi", "Plasma TV", "", "", "", "")
+    catalogue.products.push(existing_product1)
+    catalogue.products.push(existing_product2)
+    catalogue.products.push(existing_product3)
+    catalogue.products.push(existing_product4)
+
+    catalogue.search("Plasma").count.should == 3
+  end
+
+  it "should return an array of products of which each product's category contains the search term" do
+    catalogue = Catalogue.new
+    existing_product1 = Product.new(0, "", "T850", "Sony", "LCD TV", "Home Entertainment", "", "", "")
+    existing_product2 = Product.new(1, "", "X434", "LG", "Plasma TV", "Home Entertainment", "", "", "")
+    existing_product3 = Product.new(2, "", "T8000", "Sony", "Plasma TV", "Home Entertainment", "", "", "")
+    existing_product4 = Product.new(3, "", "D310", "Hitachi", "Plasma TV", "Home Entertainment", "", "", "")
+    catalogue.products.push(existing_product1)
+    catalogue.products.push(existing_product2)
+    catalogue.products.push(existing_product3)
+    catalogue.products.push(existing_product4)
+
+    catalogue.search("Entertainment").count.should == 4
+  end
+
+  it "should return an array of products of which each product's location contains the search term" do
+    catalogue = Catalogue.new
+    existing_product1 = Product.new(0, "", "T850", "Sony", "LCD TV", "Home Entertainment", "", "", "GF3A")
+    existing_product2 = Product.new(1, "", "X434", "LG", "Plasma TV", "Home Entertainment", "", "", "1F4A")
+    existing_product3 = Product.new(2, "", "T8000", "Sony", "Plasma TV", "Home Entertainment", "", "", "1F7A")
+    existing_product4 = Product.new(3, "", "D310", "Hitachi", "Plasma TV", "Home Entertainment", "", "", "5F8A")
+    catalogue.products.push(existing_product1)
+    catalogue.products.push(existing_product2)
+    catalogue.products.push(existing_product3)
+    catalogue.products.push(existing_product4)
+
+    catalogue.search("1F").count.should == 2
+  end
 end
