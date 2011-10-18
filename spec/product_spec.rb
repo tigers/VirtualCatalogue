d92f021864244dc58b1965a78223a7c8e8aa630a
firtname lastname <pantota@hotmail.com>
2011-10-18 15:49:01 +0100

    made some changes in Product class

--- spec/product_spec.rb
+++ spec/product_spec.rb
@@ -5,23 +5,29 @@ describe Product do
 
   it "should not accept any parameter to be nil" do
 
-    lambda {product = Product.new("bike", "spec", "location", 234, 23423452345)}.should_not raise_exception()
+    lambda {product = Product.new(1, 235432452345, "bike", "nomad", "super bike", "bikes", 134, "uri", "second level")}.should_not raise_exception()
 
   end
   it "id parameter should be integer" do
-    product = Product.new("bike", "spec", "location", 2345 , 23423452345)
+    product = Product.new(1, 235432452345, "bike", "nomad", "super bike", "bikes", 134, "uri", "second level")
     product.id.class.should == Fixnum
 
   end
   it "barcode parameter should be integer" do
-    product = Product.new("bike", "spec", "location", 234 , 23423452345)
+    product = Product.new(1, 235432452345, "bike", "nomad", "super bike", "bikes", 134, "uri", "second level")
     product.barcode.class.should == Bignum
 
   end
+  it "price parameter should be integer" do
+    product = Product.new(1, 235432452345, "bike", "nomad", "super bike", "bikes", 134, "uri", "second level")
+    product.price.class.should == Fixnum
+
+  end
   it "name parameter should be String" do
-    product = Product.new("bike", "spec", "location", 2345, 23423452345)
+    product = Product.new(1, 235432452345, "bike", "nomad", "super bike", "bikes", 134, "uri", "second level")
     product.name.class.should == String
 
   end
+
 
 end
