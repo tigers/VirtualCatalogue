bed48e9599c49de93834035c9a9e83beacc9fb89
Tigers <renzo.bertuzzi@postgrad.manchester.ac.uk>
2011-10-20 16:52:42 +0100

    it should load the catalogue object when app starts

--- lib/user_interface.rb
+++ lib/user_interface.rb
@@ -2,6 +2,7 @@ require 'rubygems'
 require 'sinatra'
 require 'erb'
 require 'storage'
+require 'catalogue'
 
 set :static, true
 set :public_folder, '../html'
@@ -9,6 +10,10 @@ set :public_folder, '../html'
 configure do
   storage = Storage.new
   set :my_storage, storage
+
+  catalogue = Catalogue.new
+  set :my_catalogue, catalogue
+
 end
 
 get '/' do
--- spec/user_interface_spec.rb
+++ spec/user_interface_spec.rb
@@ -23,4 +23,9 @@ describe "User Interface" do
   it "should load the storage object when it starts" do
     app.settings.my_storage.should_not be_nil
   end
+
+  it "should load the catalogue object when it starts" do
+    app.settings.my_catalogue.should_not be_nil
+  end
+
 end
