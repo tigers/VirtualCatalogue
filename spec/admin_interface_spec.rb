9776f751200ace6214e0911594957459bf902958
Despo Karagiorgi <karagiod@uf058.cs.man.ac.uk>
2011-10-29 12:03:30 +0100

    should respond to /productform when edit button is pressed and retrieve all the attributes

--- spec/admin_interface_spec.rb
+++ spec/admin_interface_spec.rb
@@ -21,6 +21,19 @@ describe "Admin Interface" do
     ["name", "barcode", "brand", "description", "price", "picture", "location"].each do | att |
       last_response.body.should include "<input type=\"text\" name=\"#{att}\" value=\"\"/>"
     end
+  end
+
+  it "should respond to /productform when 'edit' button is pressed" do
+    c = Catalogue.new
+      c.add_product Product.new(1, bc=12345678, n="lcd tv1", b="sony", d="lcd tv", cat=3, p=2000, pict="1.jpg", l="level2")
+    app.settings.my_catalogue = c
+
+    post '/productform', :submitBtn => "Edit", :product => "1"
+    last_response.should be_ok
+    last_response.body.should include "Edit Product"
+    {"name" => n, "barcode"=>bc, "brand"=>b, "description"=>d, "price"=>p, "picture"=>pict, "location"=>l}.each_pair do | att, value |
+      last_response.body.should include "<input type=\"text\" name=\"#{att}\" value=\"#{value}\"/>"
+    end
 
   end
 
